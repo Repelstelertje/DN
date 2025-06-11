8af8966ddc7737251f3f078ab13d1238f2781f5c
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-11 15:47:25 +0200

    Disable PHP error display in header

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,12 @@
   $companyName = "Dating Nebenan";
   include('includes/nav_items.php');
 
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
+  // ini_set('display_errors', 1);
+  // ini_set('display_startup_errors', 1);
+  // error_reporting(E_ALL);
+  ini_set('display_errors', 0);
+  ini_set('log_errors', 1);
+  ini_set('error_log', __DIR__ . '/../php_errorlog');
   error_reporting(E_ALL);
 ?>
 
