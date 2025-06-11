8a972c974d76a04fa5a28f0914685db8bdd0bd39
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-11 16:44:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
             </div>
         </div>
         <script nonce="2726c7f26c">
-            var api_url= "https://23mlf01ccde23.com/profile/banner9/120";
+            var api_url= "https://23mlf01ccde23.com/profile/banner/12";
         </script>
         <!-- Pagination -->
         <nav class="nav-pag" aria-label="Page navigation">
