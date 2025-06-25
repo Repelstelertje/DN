6032167046a0cd7b7dada7a1b603257092c2416e
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-25 13:46:10 +0200

    img + config

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $config = include('includes/config.php');
+    $config = include('config.php');
     if (!empty($config['DEBUG'])) {
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
