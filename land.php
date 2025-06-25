42b8b84302157f97e915596ef9c0f2a8b2f58dc1
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-25 13:10:39 +0200

    Add country navigation and improve land page

--- land.php
+++ land.php
@@ -1,6 +1,5 @@
 <?php
     include('includes/array_prov.php');
-    include('includes/header.php');
     include('includes/utils.php');
 
     $land = isset($_GET['land']) ? strip_bad_chars($_GET['land']) : '';
@@ -21,16 +20,30 @@
             $provinces = [];
             $landTitle = '';
     }
+
+    if (empty($landTitle)) {
+        header('Location: 404.php');
+        exit;
+    }
+
+    define('TITLE', 'Dating ' . $landTitle);
+    include('includes/header.php');
 ?>
 <div class="container">
   <div class="jumbotron my-4">
-    <h1 class="text-center">Provinzen in <?php echo $landTitle; ?></h1>
+    <h1 class="text-center">Dating <?php echo $landTitle; ?></h1>
     <hr>
-    <ul class="list-unstyled">
+    <div class="row">
     <?php foreach ($provinces as $slug => $prov) { ?>
-        <li><a href="provincie.php?item=<?php echo $slug; ?>"><?php echo $prov['name']; ?></a></li>
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div class="card h-100">
+                <div class="card-body text-center">
+                    <h5 class="card-title"><a href="provincie.php?item=<?php echo $slug; ?>"><?php echo $prov['name']; ?></a></h5>
+                </div>
+            </div>
+        </div>
     <?php } ?>
-    </ul>
+    </div>
   </div>
 </div>
 <?php include('includes/footer.php'); ?>
