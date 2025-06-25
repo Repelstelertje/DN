7b3df352bb0a6b5194bad3d5af22c28fb387dfba
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-25 14:18:08 +0200

    Update provincie.php

--- provincie.php
+++ provincie.php
@@ -34,7 +34,7 @@
   <div class="row" v-cloak>
     <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item" id="Slankie" v-for="profile in filtered_profiles" >
       <div class="card h-100">
-          <a :href="'<?php echo $baseUrl; ?>date-mit-' + slugify(profile.name) + '?id=' + profile.id"><img class="card-img-top" :src="profile.src.replace('150x150', '300x300')" :alt="'Dating in ' + profile.province + ' mit ' + profile.name" :title="'Siehe das Profil von ' + profile.name + ' aus ' + profile.city" @error="imgError"></a>
+          <a :href="'<?php echo $baseUrl; ?>/date-mit-' + slugify(profile.name) + '?id=' + profile.id"><img class="card-img-top" :src="profile.src.replace('150x150', '300x300')" :alt="'Dating in ' + profile.province + ' mit ' + profile.name" :title="'Siehe das Profil von ' + profile.name + ' aus ' + profile.city" @error="imgError"></a>
           <div class="card-body">
             <div class="card-top">
                 <h4 class="card-title">{{ profile.name }}</h4>  
@@ -46,7 +46,7 @@
               <li class="list-group-item">Bundesland: {{ profile.province }}</li>
             </ul>
           </div>
-          <a :href="'<?php echo $baseUrl; ?>date-mit-' + slugify(profile.name) + '?id=' + profile.id" class="card-footer btn btn-primary">Profil ansehen</a></div>
+          <a :href="'<?php echo $baseUrl; ?>/date-mit-' + slugify(profile.name) + '?id=' + profile.id" class="card-footer btn btn-primary">Profil ansehen</a></div>
       </div>
     </div>
   </div><!-- /.row -->
